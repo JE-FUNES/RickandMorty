65ba0cb1e9b061c98ecc534c12787e0d30041af7
JE-FUNES <infoventacba@gmail.com>
2023-09-22 08:32:47 -0300

    new login

--- server/src/controllers/login.js
+++ server/src/controllers/login.js
@@ -1,28 +1,55 @@
-/* crear y exportar una función que recibirá por parámetro a los 
-objetos req y res.
 
-Deberás obtener los datos email y password que recibes mediante 
-Query. Una vez hecho esto, importa tu arreglo de usuarios y 
-verifica si dentro de ese arreglo hay un usuario que coincida 
-tanto su email y su contraseña con los que recibes por Query.
 
-En el caso de que haya un usuario que cumpla esa condición, 
-entonces debes devolver una respuesta con status 200, y, 
-en formato JSON, un objeto con una propiedad access: true. 
-Caso contrario devuelve lo mismo pero con la propiedad 
-access: false.
+const users = require('../models/User');
+
+/* Crea una función llamada login la cual reciba por parámetro los objetos 
+req y res. No olvides exportarla.
+
+Recibiras por Query los datos email y password.
+
+En el caso de no recibir alguno de los datos, responde con un status 400 y el 
+mensaje "Faltan datos".
+
+Si ambos datos llegan correctamente tendrás que buscar aquel usuario que tenga 
+el mismo email que recibiste anteriormente. En el caso de no encontrarlo 
+responde con un status 404 y el mensaje "Usuario no encontrado".
+
+En el caso de encontrar a un usuario con ese mismo email solo tendrás ahora 
+que comparar si su password es igual a la password que recibiste anteriormente. 
+En el caso de no serlo responde con un status 403 y un mensaje que diga 
+"Contraseña incorrecta".
+
+En el caso de que las contraseñas si coincidan, responde con el objeto:
+
+{
+   access: true;
+}
+[NOTA]: en el caso de haber un error responde con status 500 y el mensaje del 
+error.
 */
 
-const users = require('../utils/users');
-
-const login = (req, res) => {
-  const { email, password } = req.query;
-  const user = users.find(user => user.email === email && user.password === password);
-  if (user) {
-    res.status(200).json({ access: true });
-  } else {
-    res.status(200).json({ access: false });
-  }
-}
+const login = async (req, res) => {
+    const { email, password } = req.query;
+    if (!email || !password) {
+        res.status(400).json({ message: 'Faltan datos' });
+    } else {
+        try {
+        const user = await users.findOne({
+            where: { email },
+        });
+        if (!user) {
+            res.status(404).json({ message: 'Usuario no encontrado' });
+        } else {
+            if (user.password !== password) {
+            res.status(403).json({ message: 'Contraseña incorrecta' });
+            } else {
+            res.status(200).json({ access: true });
+            }
+        }
+        } catch (error) {
+        res.status(500).json({ message: error.message });
+        }
+    }
+    };
 
 module.exports = login;
